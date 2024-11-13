df64277be2e49373ee034e66e513647dc3933fe7
Abhishek Singh <you@example.com>
2024-11-13 20:18:57 +0530

    seekbar completed

---
+++ script.js
@@ -0,0 +1,7 @@
+async function main(params) {
+    let currentSongs;
+    Array.from(document.querySelector(".songList").getElementsByTagName("li")).forEach((e) => {
+        console.log(e);
+    })
+}
+main()  
